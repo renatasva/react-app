c2768facf1920073bb2a5ea70103265de0e57bf5
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-11 18:21:44 +0000

    counter completed

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,28 +2,25 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 2 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
+  render() {
+    const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props; 
 
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c=> c.id !== counterId);
-    this.setState( { counters })
-  }
-
-  render() { 
     return ( 
     <div>
-      { this.state.counters.map(counter => (
+      <button 
+        // onClick={this.props.onReset}
+        onClick={onReset}
+        className="btn btn-primary btn-sm m-2">Reset</button>
+      {/* { this.props.counters.map(counter => ( */}
+      { counters.map(counter => (
         <Counter 
           key={counter.id} 
-          onDelete ={this.handleDelete} 
-          counter = {counter}
+          // onDelete={this.props.onDelete}
+          onDelete={onDelete} 
+          // onIncrement={this.props.onIncrement}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          counter={counter}
         />
     ))}
     </div>
